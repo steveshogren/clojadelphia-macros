cd46e71a773e0715cd36ae542100e491bf86d3f9
Steve Shogren <steve.a.shogren@gmail.com>
2013-10-31 17:53:12 -0400

    Additional cleaning up the presentation file

--- presentation.clj
+++ presentation.clj
@@ -1,8 +1,6 @@
-(ns dwarffortress.presentation)
-
 ;; Templates
 
-;; Think about templates for a second...
+;; Think about templater a second...
 <html>
   <p>
   <?php> if(today_day() == "Monday") { echo("Mon") } </?>
@@ -34,7 +32,7 @@
 
 
 
-
+`(+ 1 1)
 
 
 ;; no quote                +   => #<core$_PLUS_ clojure.core$_PLUS_@d3d424c>
@@ -54,7 +52,7 @@
 ;; "Replaced"
 (defn add [2 3]
   (+ 2 3))
-
+; 
 
 ;; Macro 
 (defmacro add [x y]
@@ -64,11 +62,11 @@
 ;; After "replacing" the values
 (defmacro add [(+ 1 1) 3]
   `(+ (+ 1 1) 3))
-;; (macroexpand '(add (+ 1 1) 3)) => (clojure.core/+ (+ 1 1) 3)
+;; (pprint (macroexpand '(add (+ 1 1) 3))) => (clojure.core/+ (+ 1 1) 3)
 
 
-;; Macros allow for the expansion of code -without- the
-;; increase in stack depth and associated attributes
+;; Macros allow for the expansion of code as if there was
+;; no increase in stack depth
 
 
 
@@ -85,23 +83,24 @@
 
 ;; ---- Game probability macros ----
 
-;; revert to just chance after figuring out what is wrong with '50% and '50x
 (defn chance [x]
   (> x (rand-int 100)))
 
+(average-out #(if (chance 50) 1 100) 10000)
 
 (defmacro make-percents []
   "(ifN x y) returns x N% of the time, but ensures conditional evaluation, like 'if'"
   `(list ~@(map (fn [num] 
-            (let [macro-name (symbol (str "if" num))]
-              `(defmacro ~macro-name [x# y#]
-                 `(if (> ~~num (rand-int 100)) ~x# ~y#))))
-          (range 100))))
-
-;; (make-percents) => (defmacro if0 ...) (defmacro if1 ...) ...
+                  (let [macro-name (symbol (str "if" num))]
+                    `(defmacro ~macro-name [x# y#]
+                       `(if (chance ~~num) ~x# ~y#))))
+                (range 1 100))))
+;
+;;(make-percents)
+;; (pprint (macroexpand '(make-percents))) => (defmacro if0 ...) (defmacro if1 ...) ...
+;; (if99 1 100)
 ;; (pprint (macroexpand '(if1 :x :y)))
 ;; (average-out #(if50 1 100) 10000)   
-
 
 
 
@@ -121,7 +120,7 @@
 (if-percent 50 :a, 45 :b, 5 :c)
 
 (if-percent-b [50 :a] [45 :b] [5 :c])
-
+;
 (if-percent-c :a 50 :b 45 :c 5)
 
 (return-a 50 % chance of :a
@@ -134,18 +133,41 @@
 
 
 
+
+
+
+
+
+
+
+
+
             
 ;; So these are cool, but how do we make them work on seqs?
-(defmacro adder [x y] `(+ ~x ~y))
+(defmacro adder [x y]
+  `(+ ~x ~y))
+
 (reduce adder 0 [4 5])
 ;; => java.lang.RuntimeException: Can't take value of a macro: #'user/adder
 
-
+;
 ;; We make a regular function syntax that might be unwieldy, but allowing at
 ;; least the _ability_ to use over collections
 (if-percent-fn 50 (fn [] :a)
                49 (fn [] :b)
                1 (fn [] :c))
+
+
+
+
+
+
+
+
+
+
+
+
 
 (defn if-percent-fn [& n]
   " Takes a set of percent chances and return functions
@@ -170,47 +192,70 @@
                 (recur current-sum (rest items))))))
         (throw (Exception. (str "Nums: " sum " didn't equal 100" )))))))
 
-
-
-
-
-
 ;; This frees us now to explore shorter or more expressive syntaxes
 
 
 
 
 
-(defn convert-to-num [x]
-  (if (number? x) x
-      (-> x str count (* 5))))
+
+
+
+
+
 
 (defmacro if-percent [& n]
-  "(if-percent 50 :a 49 :b 1 :c) returns :a 50% of the time, :b 49%, and :c 1%
-   (if-percent ********** :a
-               *********  :b
-               *          :c) => 50% :a, 45% :b, 5% :c "
+  "(if-percent 50 :a 49 :b 1 :c) returns :a 50% of the time, :b 49%, and :c 1%"
   (if (even? (count n))
     (let [pairs (partition 2 n)
           pairs (mapcat (fn [[pct act]]
-                          [(convert-to-num pct) `(fn [] ~act)])
+                          [pct `(fn [] ~act)])
                         pairs)]
       `(if-percent-fn ~@pairs))))
 
-
+(pprint (macroexpand '(if-percent 50 1 50 100)))
 ;; (average-out #(if-percent 50 1 50 100) 10000)   
+;; (average-out #(if-percent ************** 1 ****** 100) 10000)   
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 ;; ---- Anaphoric Macros ----
 
+
+
+(let [x 5]
+  (let [x 4]
+    x))
+
+
+((fn [x]
+   ((fn [x]
+      x)
+    4))
+ 5)
+
+
 ;; Here we shadow the z value with another
 (defmacro add-weird [x]
   `(let [~'z 100]
      ~x))
+
 (let [z 1]
   (add-weird (inc z))) ;; => 101 ...not 2 
+
+
 
 
 
@@ -238,10 +283,16 @@
       (average _) 
       (float _))
 
-(pprint (macroexpand '(_> (range 1000)
+(pprint (macroexpand
+         '(_> (range 1000)
+              (map (fn [x] (if-percent 50 1 50 100)) _)
+              (average _) 
+              (float _))))
+(let [x  (_> (range 1000)
        (map (fn [x] (if-percent 50 1 50 100)) _)
        (average _) 
-       (float _))))
+       (float _))]
+  _)
 
 
 ;; Transliteration of PG's aif
