27f8f9d3348f60a97b074ecd4f6a0f785c2b48f2
Steve Shogren <steve.a.shogren@gmail.com>
2015-06-27 06:57:58 -0400

    Links to put on the meetup site

---
+++ workshop_links.clj
@@ -0,0 +1,33 @@
+Links To Topics:
+
+Channels / Goroutines
+Core.Async:
+https://github.com/clojure/core.async
+
+Editing
+Paredit Examples:
+http://danmidwood.com/content/2014/11/21/animated-paredit.html
+Spacemacs:
+https://github.com/syl20bnr/spacemacs
+
+Introductory Exercises
+4Clojure
+https://www.4clojure.com/
+
+Types / Typeshaping / Compiling
+Schema:
+https://github.com/Prismatic/schema
+Core.Typed:
+https://github.com/clojure/core.typed
+
+Web Microframework
+Luminus
+http://www.luminusweb.net/
+
+Javascript Hotswapping
+Figwheel
+https://github.com/bhauman/lein-figwheel
+
+Java Swing DSL
+Seesaw
+https://github.com/daveray/seesaw
