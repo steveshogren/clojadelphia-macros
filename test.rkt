9c7205ba70dffbb524b1155aebec969a43c62b4c
Jack <steve.a.shogren@gmail.com>
2015-12-03 06:49:42 -0500

    More sicp madness!

--- test.rkt
+++ test.rkt
@@ -11,18 +11,35 @@
   (check-equal? (add 1 2) 3)
   (check-equal? (add 2 2) 4)
 
-  (define (work-day? day hour)
-    (cond ((and (= hour 8)
-                (or (= day 2)
-                    (= day 3)
-                    (= day 4)
-                    (= day 5)
-                    (= day 6))) true)
+
+  (define (new-work-day? day hour)
+    (cond ((or (and (= day 2)
+                    (= hour 8))
+               (and (= day 3)
+                    (= hour 8))) true)
           (true false)))
-  (check-equal? (work-day? 1 2) false)
-  (check-equal? (work-day? 2 2) false)
-  (check-equal? (work-day? 2 8) true)
-  (check-equal? (work-day? 2 9) false)
-  (check-equal? (work-day? 5 8) true)
-  (check-equal? (work-day? 5 5) false)
+  (check-equal? (new-work-day? 1 2) false)
+  (check-equal? (new-work-day? 1 8) false)
+  (check-equal? (new-work-day? 2 8) true)
+  (check-equal? (new-work-day? 3 8) true)
+
+
+  (check-equal? (a-plus-abs-b 3 2) 5)
+
+  (define (negative? a) (< a 0))
+
+  (define add +)
+  (define sub -)
+
+  (define (a-plus-abs-b a b)
+    ((if (negative? b) sub add) a b))
+
+  (define (a-plus-abs-b-lame a b)
+    (if (negative? b)
+        (- a b)
+        (+ a b)))
+
+  (check-equal? (a-plus-abs-b-lame 3 2) 5)
+  (check-equal? (a-plus-abs-b-lame 3 -2) 5)
   )
+
