fda69464d9312fb6f060dc81acbb1cd97526a292
Jack <steve.a.shogren@gmail.com>
2015-12-04 06:52:29 -0500

    Working through the newton square root problem

--- test.rkt
+++ test.rkt
@@ -5,41 +5,33 @@
   (require (planet "main.rkt" ("neil" "sicp.plt" 1 (= 17)))
            rackunit)
 
-  (define (add x y)
-    (+ x y))
+  (define (square x)
+    (* x x))
 
-  (check-equal? (add 1 2) 3)
-  (check-equal? (add 2 2) 4)
+  (define (average x y)
+    (/ (+ x y) 2))
 
+  (define (improve guess x)
+    (average guess (/ x guess)))
 
-  (define (new-work-day? day hour)
-    (cond ((or (and (= day 2)
-                    (= hour 8))
-               (and (= day 3)
-                    (= hour 8))) true)
-          (true false)))
-  (check-equal? (new-work-day? 1 2) false)
-  (check-equal? (new-work-day? 1 8) false)
-  (check-equal? (new-work-day? 2 8) true)
-  (check-equal? (new-work-day? 3 8) true)
+  ;;(check-equal? (improve 1 2) 3/2)
+  (define (new-if predicate then-clause else-clause)
+    (if predicate
+        then-clause
+        else-clause))
 
+  (define (sqrt-iter guess x)
+    (new-if (good-enough? guess x)
+        guess
+        (sqrt-iter (improve guess x) x)))
 
-  (check-equal? (a-plus-abs-b 3 2) 5)
+  (define (good-enough? guess x)
+    (< (abs (- (square guess) x)) 0.001))
 
-  (define (negative? a) (< a 0))
+  (define (sqrt x)
+    (sqrt-iter 1.0 x))
 
-  (define add +)
-  (define sub -)
+  ;(sqrt 2)
 
-  (define (a-plus-abs-b a b)
-    ((if (negative? b) sub add) a b))
-
-  (define (a-plus-abs-b-lame a b)
-    (if (negative? b)
-        (- a b)
-        (+ a b)))
-
-  (check-equal? (a-plus-abs-b-lame 3 2) 5)
-  (check-equal? (a-plus-abs-b-lame 3 -2) 5)
   )
 
