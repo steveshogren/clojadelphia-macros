1f2696f3b89a2dd6d2fc43dbe644cae225649835
Jack <steve.a.shogren@gmail.com>
2015-12-20 14:57:08 -0500

    Cleaning a bit more

--- romannumbers.clj
+++ romannumbers.clj
@@ -1,23 +1,21 @@
 
 
+(def map-num-to-string
+  {1 "I"
+   4 "IV"
+   5 "V"
+   9 "IX"
+   10 "X"
+   40 "XL"
+   50 "L"
+   90 "XC"
+   100 "C"
+   400 "CD"
+   500 "D"
+   900 "CM"
+   1000 "M"})
 
-(defn map-num-to-string [number]
-  (case number
-    1 "I"
-    4 "IV"
-    5 "V"
-    9 "IX"
-    10 "X"
-    40 "XL"
-    50 "L"
-    90 "XC"
-    100 "C"
-    400 "CD"
-    500 "D"
-    900 "CM"
-    1000 "M"))
-
-(def increments [1 4 5 9 10 40 50 90 100 400 500 900 1000])
+(def increments (keys map-num-to-string))
 
 (defn next-letter [arabic]
   (->> increments
@@ -31,7 +29,7 @@
       ret
       (let [next (next-letter arabic)]
         (recur (- arabic next)
-               (str ret (map-num-to-string next)))))))
+               (str ret (get map-num-to-string next)))))))
 
 (to-roman 29)
 (to-roman 448) ; CDXLVIII 
