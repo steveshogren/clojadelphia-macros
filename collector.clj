aef2fb4c7734867fa4e4a9d53e7d966074eddd18
Steve Shogren <steve.a.shogren@gmail.com>
2020-01-26 16:35:22 -0500

    determine each roll is within a tolerance

--- collector.clj
+++ collector.clj
@@ -12,10 +12,14 @@
 
 (defn is-balanced [rolls]
   (let [counts (map val rolls)
-        average (/ (reduce + counts) (count counts))]
-    average)
-  )
-(->> (range 0 100)
+        average (/ (reduce + counts) (count counts))
+        tolerance (* average 0.1)]
+    (map (fn [cnt]
+           (let [in-tolerance (> (+ average tolerance) cnt (- average tolerance))]
+             [in-tolerance average tolerance cnt]))
+         counts)))
+
+(->> (range 0 10000)
      (map (fn [_] (roll-d10)))
      (collector)
      (sorter)
