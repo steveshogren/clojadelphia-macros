5080026de7c5a7859406a120c8b9e0e72a4ce3eb
Steve Shogren <steve.a.shogren@gmail.com>
2020-02-07 07:41:42 -0500

    Overlaps function

--- range.clj
+++ range.clj
@@ -10,12 +10,22 @@
   [a b])
 
 (defn allPoints_ [[a b]]
-  (range a (inc b))
-  )
+  (range a (inc b)))
+
+(defn overlaps_ [[a b] other]
+  (reduce (fn [ret next] (or ret (and (<= next b) (<= a next))))
+          false
+          (allPoints other)))
+
 
 (deftest rangeTest
   (testing "all points"
     (is (= [1 2 3] (allPoints_ (range_ 1 3))))
+    )
+  (testing "overlaps range"
+    (is (= true (overlaps_ (range_ 0 2) (range_ 1 3))))
+    (is (= false (overlaps_ (range_ 0 2) (range_ 3 4))))
+    (is (= true (overlaps_ (range_ 2 6) (range_ 1 3))))
     )
   (testing "contains_"
     (is (= true (contains_ [2 5] (range_ 1 7))))
