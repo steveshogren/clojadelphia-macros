de79ef55fee614c2aa822995adcbc5f6b22eb2cf
Steve Shogren <steve.a.shogren@gmail.com>
2020-02-14 09:18:19 -0500

    basic delete from ordered tree

--- dfs.clj
+++ dfs.clj
@@ -11,17 +11,67 @@
     (let [[node l r] tree]
       (if (< v node)
         [node (insert l v) r]
-        ))
-    )
+        [node l (insert r v)]))))
+
+(defn delete [tree v]
+  (if (= [] tree) tree
+      (let [[node l r] tree]
+        (if (= node v)
+          (cond
+            (and (= l []) (= r [])) []
+            (not= l []) (let [[lv ll lr] l]
+                          [lv ll r])
+            true (let [[rv rl rr] r]
+                          [rv l rr])
+            )
+          (if (< v node)
+            [node (delete l v) r]
+            [node l (delete r v)]
+            ))
+          )
+        )
   )
 
 (deftest dfsTest
+  (testing "delete from tree"
+    (is (= [3 [] []] (delete [3 [2 [] []] []] 2)))
+
+    (is (= [6
+            [4
+             []
+             []]
+            []]
+           (delete [6
+                    [5
+                     []
+                     [4 [] []]]
+                    []]
+                   5)))
+
+    (is (= [6
+            [2
+             []
+             [4 [] []]]
+            []]
+           (delete [6
+                    [5
+                     [2 [] []]
+                     [4 [] []]]
+                    []]
+                   5)))
+
+    (is (= [] (delete [] 2)))
+    )
   (testing "make a tree"
     (is (= [1 [] []] (insert [] 1)))
-    (is (= [3 [2 [] []] []] (insert (insert [] 3)
-                               2)))
-    )
-
-  )
+    (is (= [3
+            [2 [] []]
+            []]
+           (insert (insert [] 3) 2)))
+    (is (= [3
+            []
+            [4 [] []]]
+           (insert (insert [] 3) 4)))
+    ))
 
 (run-tests)
