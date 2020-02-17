a628353c4119780377b9c72dfc4bd2f6e3927042
Steve Shogren <steve.a.shogren@gmail.com>
2020-02-17 06:11:13 -0500

    wip on a pretty printer

--- dfs.clj
+++ dfs.clj
@@ -49,10 +49,34 @@
         ))
   )
 
+(defn pretty-print [tree]
+  (loop [nodes []
+         queue [tree]
+         total-depth 0]
+    (let [tree (first queue)
+          newNode (if (and (vector? tree)
+                           (not= [] tree)) tree [tree [] []])
+          [vv vl vr] newNode]
+      (if (= [] vv)
+        nodes
+        (do
+          (recur (conj nodes vv)
+                 (conj (conj queue vl) vr)
+                 (inc total-depth)
+                 )
+            ))))
+  )
+
+;; (pretty-print [5 [2 [] []] [9 [] []]])
+
+;;(pretty-print [6 [5 [2 [] []] [4 [] []]] []])
+
+
 (deftest dfsTest
   (testing "delete from tree"
     (is (= [3 [] []] (delete [3 [2 [] []] []] 2)))
-    
+
+    ;; promotes right child if no left
     (is (= [6
             [4
              []
@@ -64,7 +88,8 @@
                      [4 [] []]]
                     []]
                    5)))
-    
+
+    ;; promotes left child if exists
     (is (= [6
             [2
              []
@@ -76,20 +101,20 @@
                      [4 [] []]]
                     []]
                    5)))
-    
-    (is (= [6
+
+    ;; balances children by moving left child's right node to right
+    (is (= [7
             [2
              [1 [] []]
-             [4 [3 [] []] []]]
+             [6 [3 [] []] []]]
             []]
-           (delete [6
+           (delete [7
                     [5
                      [2 [1 [] []] [3 [] []]]
-                     [4 [] []]]
+                     [6 [] []]]
                     []]
                    5)))
-    
-    
+
     (is (= [] (delete [] 2)))
     )
   (testing "make a tree"
